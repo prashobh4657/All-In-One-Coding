181d8c1c7a2e43ca70fd0151350aad354fddaa4e
prashobh173 <prashobh.1@cars24.com>
2025-11-16 09:28:01 +0530

    All-in-one coding basics

---
+++ All-in-one-Coding-Questions/Basics/Swap2Numbers.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define mod 1000000007
+#define endl "\n"
+#define fio                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+template <typename T>
+istream &operator>>(istream &input, vector<T> &v)
+{
+    for (auto &i : v)
+        cin >> i;
+    return input;
+}
+template <typename T>
+ostream &operator<<(ostream &output, vector<T> &v)
+{
+    for (auto &i : v)
+        cout << i << " ";
+    return output;
+}
+int32_t main()
+{
+    fio;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        swap(a, b);
+        cout << a << " " << b << endl;
+    }
+    return 0;
+}
